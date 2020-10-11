f677629e441b5274efb7e365f60cb52a4c3bf9db
daniel-dorroh <daniel.dorroh@gmail.com>
2020-10-11 09:45:57 -0500

    Added sound assets and set up thrust and maneuvering sounds

--- RocketGame/Assets/Scenes/RocketMotion.cs
+++ RocketGame/Assets/Scenes/RocketMotion.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RocketMotion : MonoBehaviour
 {
+    public float ThrustForce = 20.0f;
+    public float ManeuverAngle = 50.0f;
     public Rigidbody Rocket;
-    public float Thrust = 14.0f;
-    public float ManeuverAngle = 10.0f;
+    public AudioSource RocketPulse;
+    public AudioSource ManeuverBlast;
+    private bool _hasManeuveredLeft = false;
+    private bool _hasManeuveredRight = false;
 
     public void Start()
     {
@@ -15,27 +16,72 @@ public class RocketMotion : MonoBehaviour
         {
             Rocket = GetComponent<Rigidbody>();
         }
+        if (RocketPulse == null)
+        {
+            RocketPulse = GetComponent<AudioSource>();
+        }
+        if (ManeuverBlast == null)
+        {
+            ManeuverBlast = GetComponent<AudioSource>();
+        }
     }
 
     public void Update()
     {
-        ProcessInputs();
+        Thrust();
+        Maneuver();
     }
 
-    private void ProcessInputs()
-    {
+    private void Thrust()
+    { 
         if (Input.GetKey(KeyCode.Space))
         {
-            Rocket.AddRelativeForce(new Vector3(0, Thrust, 0));
-        } 
+            Rocket.AddRelativeForce(new Vector3(0, ThrustForce, 0));
 
+            if (!RocketPulse.isPlaying)
+            {
+                RocketPulse.time = Random.value * 2.0f;
+                RocketPulse.loop = true;
+                RocketPulse.Play();
+            }
+        }
+        else
+        {
+            RocketPulse.Stop();
+        }
+    }
+
+    private void Maneuver()
+    {
         if (Input.GetKey(KeyCode.A))
         {
+            _hasManeuveredRight = false;
             transform.Rotate(Vector3.forward, ManeuverAngle * Time.deltaTime);
-        } 
+            _hasManeuveredLeft = PlayManeuverSound(_hasManeuveredLeft);
+        }
         else if (Input.GetKey(KeyCode.D))
         {
+            _hasManeuveredLeft = false;
             transform.Rotate(Vector3.back, ManeuverAngle * Time.deltaTime);
+            _hasManeuveredRight = PlayManeuverSound(_hasManeuveredRight);
         }
+        else
+        {
+            ManeuverBlast.Stop();
+            _hasManeuveredRight = false;
+            _hasManeuveredLeft = false;
+        }
+    }
+
+    private bool PlayManeuverSound(bool hasManeuvered)
+    {
+        if (!hasManeuvered && !ManeuverBlast.isPlaying)
+        {
+            hasManeuvered = true;
+            ManeuverBlast.time = 0.4f;
+            ManeuverBlast.loop = false;
+            ManeuverBlast.Play();
+        }
+        return hasManeuvered;
     }
 }
