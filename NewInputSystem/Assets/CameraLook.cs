1fe2428d1f7c0df60d5a183fa302b7eb00b1c533
daniel-dorroh <daniel.dorroh@gmail.com>
2020-10-29 17:42:09 -0500

    Created project dedicated to experimenting and learning new unity input system

---
+++ NewInputSystem/Assets/CameraLook.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraLook : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float turnSpeed = 0.5f;
+    [SerializeField] private float nodSpeed = 0.5f;
+
+    private Controls _controls;
+    private float _verticalAngle;
+    private float _horizontalAngle;
+    private int _ignoreCount = 0;
+
+    private const float MaximumVerticalAngle = 89.5f;
+
+    void Awake()
+    {
+        _controls = new Controls();
+    }
+
+    void Start()
+    {
+        if (player == null)
+        {
+            throw new InvalidOperationException("player must be initialized");
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        _verticalAngle = 0;
+        _horizontalAngle = 0;
+    }
+
+    void Update()
+    {
+        var look = _controls.Player.Look.ReadValue<Vector2>();
+        Look(look);
+    }
+
+    private void Look(Vector2 mouseDeltas)
+    {
+        if (_ignoreCount < 10)
+        {
+            _ignoreCount++;
+            return;
+        }
+
+        if (mouseDeltas.sqrMagnitude < 0.01)
+        {
+            return;
+        }
+
+        var nod = nodSpeed * Time.deltaTime;
+        var turn = turnSpeed * Time.deltaTime;
+
+        _verticalAngle += mouseDeltas.y * nod;
+        _verticalAngle = Mathf.Clamp(_verticalAngle, -MaximumVerticalAngle, MaximumVerticalAngle);
+
+        _horizontalAngle = mouseDeltas.x * turn;
+
+        transform.localRotation = Quaternion.Euler(_verticalAngle, 0.0f, 0.0f);
+        player.Rotate(Vector3.up, _horizontalAngle);
+    }
+
+    void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        _controls.Disable();
+    }
+}
---
+++ NewInputSystem/Assets/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float groundSpeed = 15.0f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.1f;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float jumpHeight = 1.5f;
+    [SerializeField] private Camera eyes;
+
+    private Controls _controls;
+    private Vector3 _movementDirection = Vector3.zero;
+    private Vector3 _velocity = Vector3.zero;
+    private bool _isGrounded = false;
+
+    private const float MinimumDownwardVelocity = 0.0f;
+
+    void Awake()
+    {
+        _controls = new Controls();
+        _controls.Player.Move.performed += OnMovePerformed;
+        _controls.Player.Move.canceled += OnMoveCanceled;
+        _controls.Player.Jump.performed += OnJumpPerformed;
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext obj)
+    {
+        if (_isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(-2.0f * jumpHeight * Physics.gravity.y);
+        }
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (_isGrounded && _velocity.y < MinimumDownwardVelocity)
+        {
+            _velocity.y = MinimumDownwardVelocity;
+        }
+
+        var horizontalVelocity =
+            groundSpeed * Vector3.ProjectOnPlane(
+                    eyes.transform.TransformVector(_movementDirection),
+                    Vector3.up)
+            .normalized;
+
+        controller.Move((_velocity + horizontalVelocity) * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        _velocity += Physics.gravity * Time.fixedDeltaTime;
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+        _movementDirection.x = 0.0f;
+        _movementDirection.z = 0.0f;
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext obj)
+    {
+        var input = obj.ReadValue<Vector2>();
+        _movementDirection.x = input.x;
+        _movementDirection.z = input.y;
+    }
+
+    void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        _controls.Disable();
+    }
+}
