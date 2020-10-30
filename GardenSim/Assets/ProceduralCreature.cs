6621813e815c8680f06c9a982f2b7f6c5efd02d1
daniel-dorroh <daniel.dorroh@gmail.com>
2020-10-30 15:34:57 -0500

    Added motion and color to the procedural creatures

--- GardenSim/Assets/ProceduralCreature.cs
+++ GardenSim/Assets/ProceduralCreature.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -18,6 +19,13 @@ public class Creature
     private BodyShape _bodyShape;
     private bool _isFlying;
     private GameObject _root;
+    private Color _color;
+    private float _speed;
+    private Vector3 _lastMove;
+    private Rigidbody _rigidBody;
+    private float _mass;
+
+    private const float MinimumMass = 0.1f;
 
     public Creature()
     {
@@ -26,18 +34,58 @@ public class Creature
         _scale = ReadAsNormalizedFloat(13);
         _bodyShape = ReadAsEnum<BodyShape>(22);
         _isFlying = ReadAsBool(30);
+        _mass = ReadAsNormalizedFloat(5) * 2.0f * _scale + MinimumMass;
+        if (_isFlying)
+        {
+            _mass /= 3.0f;
+        }
+        _speed = ReadAsNormalizedFloat(16) / (_scale * _mass);
 
-        var root = new GameObject();
+        _root = new GameObject();
         var body = RenderBody(_bodyShape);
-        body.transform.parent = root.transform;
-        root.transform.localScale = Vector3.one * _scale;
+        body.transform.parent = _root.transform;
+        _root.transform.localScale = Vector3.one * _scale;
         var position = Random.insideUnitSphere * 50.0f;
         position.y =  Mathf.Clamp(position.y, 2.0f, 50.0f);
-        root.transform.position = position;
+        _root.transform.position = position;
+        _rigidBody = body.AddComponent<Rigidbody>();
+        _rigidBody.mass = _mass;
+
+        Renderer bodyRenderer = body.GetComponent<Renderer>();
+        var material = new Material(Shader.Find("Standard"));
+        material.color = ReadAsRgbColor(5);
+        material.SetFloat("_Metallic", ReadAsNormalizedFloat(7));
+        material.SetFloat("_Glossiness", ReadAsNormalizedFloat(19));
+        bodyRenderer.material = material;
+    }
+
+    public Vector3 Position => _rigidBody.transform.position;
+
+    public GameObject Root => _root;
+
+    public void Move()
+    {
+        if (Random.value < 0.6)
+        {
+            return;
+        }
+
+        if (_lastMove == Vector3.zero)
+        {
+            _lastMove = Random.insideUnitSphere;
+        }
+
+        var current = _lastMove + Vector3.one * Random.value;
+        current = current.normalized;
+
         if (!_isFlying)
         {
-            body.AddComponent<Rigidbody>();
+            current.y = 0;
         }
+
+        _rigidBody.AddForce(current * _speed);
+
+        _lastMove = current;
     }
 
     private GameObject RenderBody(BodyShape bodyShape)
@@ -54,6 +102,18 @@ public class Creature
             default:
                 return GameObject.CreatePrimitive(PrimitiveType.Sphere);
         }
+    }
+
+    private Color ReadAsRgbColor(int offset)
+    {
+        var red = ReadAsNormalizedFloat(offset) * 3.5f;
+        offset += 8;
+        var green = ReadAsNormalizedFloat(offset) * 3.5f;
+        offset += 8;
+        var blue = ReadAsNormalizedFloat(offset) * 3.5f;
+        offset += 8;
+        var alpha =  ReadAsNormalizedFloat(offset);
+        return new Color(red, blue, green, alpha);
     }
 
     private bool ReadAsBool(int offset)
@@ -108,18 +168,34 @@ public enum BodyShape
 
 public class ProceduralCreature : MonoBehaviour
 {
+    private readonly List<Creature> _creatures = new List<Creature>();
     // Start is called before the first frame update
     void Start()
     {
         for (var i = 0; i < 100; i++)
         {
-            new Creature();
+            _creatures.Add(new Creature());
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        var delete = new Queue<Creature>();
+        foreach (var creature in _creatures)
+        {
+            if (creature.Position.magnitude > 500.0f)
+            {
+                delete.Enqueue(creature);
+                continue;
+            }
+            creature.Move();
+        }
+        while (delete.Count > 0)
+        {
+            var deleted = delete.Dequeue();
+            _creatures.Remove(deleted);
+            Destroy(deleted.Root);
+        }
     }
 }
